1311518885c7d2d648f6d00f86d40d92ea620ce7
tdunst83 <thomas.dunst@avalanchez.de>
2019-03-18 16:18:53 +0000

    Commented out code to make test fail for jenkins
    command added

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -2,11 +2,11 @@ package com.develogical.app;
 
 public class QueryProcessor {
     public String process(String query) {
-       /* if (query.toLowerCase().contains("shakespeare")) {
+        if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        }*/
+        }
         if (query.toLowerCase().contains("thomas")) {
             return "Wi16) was an " ;
         }
